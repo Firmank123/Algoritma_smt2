3b4fda122507c9eaf27c5f983f12834fbddc1c63
Firmank123 <firmankediri123@gmail.com>
2024-02-21 12:08:25 +0700

    Initial commit
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ buku16.java
@@ -0,0 +1,26 @@
+package week2;
+
+public class buku16 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Halaman: " + halaman);
+        System.out.println("Sisa stok: " + judul);
+        System.out.println("Harga: " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int jml) {
+        harga = hrg;
+    }
+}
