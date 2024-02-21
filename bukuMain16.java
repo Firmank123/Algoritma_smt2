de44c168c1599a65e570d7d6b0495a44452978d6
Firmank123 <firmankediri123@gmail.com>
2024-02-21 12:48:34 +0700

    Import bukuMain16
    * Main class
    
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ bukuMain16.java
@@ -0,0 +1,17 @@
+package week2;
+
+public class bukuMain16 {
+    public static void main(String[] args) {
+        buku16 bk1 = new buku16();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 192;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+    }
+}
