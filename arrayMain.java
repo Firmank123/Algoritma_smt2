ba61db23a253b38b027e457b1a9376ff9fd4f03b
Firmank123 <firmankediri123@gmail.com>
2024-02-28 22:32:16 +0700

    Import Jobsheet1 array
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

---
+++ arrayMain.java
@@ -0,0 +1,14 @@
+package job1;
+
+public class arrayMain {
+    public static void main(String[] args) {
+        System.out.println("=========================================================================================================");
+        System.out.println("                                   Program Menghitung IP Semester                                        ");
+        System.out.println("=========================================================================================================");
+
+        array ip = new array();
+        ip.inputNilai();
+        ip.hitung();
+        ip.hasil();
+    }
+}
